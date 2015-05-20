cf4dffae5a7a9fa98cd8b067a00768ea6a93230a
Dirk Link <dlink87@MacBook-Pro.local>
2015-05-20 10:05:17 -0400

    first 2 tests passed

--- coffee.rb
+++ coffee.rb
@@ -1,3 +1,16 @@
 class Coffee
-  # Add code here
+  def initialize name
+  	@name = name
+  	@vol = 5
+  end
+
+  def full?
+  	if @vol == 5
+  	end
+  end
+
+  def empty?
+  	if @vol == 0
+  	end
+  end
 end
--- human.rb
+++ human.rb
@@ -1,3 +1,25 @@
 class Human
-  # Add code here
+	def initialize name
+		@name = name
+	end
+
+	def has_coffee?
+		false
+	end
+
+	def needs_coffee?
+		true
+	end
+
+	def alertness
+		alertness = 0.0
+	end
+
+	def buy
+
+	end
+
+	def drink
+
+	end
 end
